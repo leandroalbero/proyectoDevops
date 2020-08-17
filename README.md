acaeeec42c1c4889a5021d8483f0af70c532d3de
leandroalbero <leandro80gb@gmail.com>
2020-08-18 01:52:30 +0200

    -Creado Dockerfile -Creado servidor go, de momento tan solo muestra "hola"

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Initial commit
+Construir contenedor con Dockerfile:
+    docker build . -t goserver:1.0
+Ejecutar el contenedor:
+    docker run -p 80:80 -d --name goserver goserver:1.0
+
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hola,")
+}
+
+func main() {
+
+    http.HandleFunc("/", hello)
+    http.ListenAndServe(":80", nil)
+}
